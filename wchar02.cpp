b79f6cb03ecc7f59e08029f5100d3e7fc380f3cd
Yutae <cyt9772@naver.com>
2021-09-08 00:25:10 +0900

    wchar changing

--- wchar02.cpp
+++ wchar02.cpp
@@ -1,32 +1,21 @@
 #include <iostream>
 #include <cstring>
+#include <locale>
 
 using namespace std;
 
 int main(){
-    char en[]="Hello World!";
-    cout<<"char"<<en<<endl;
-
-    cout<<"입력문자 : ";
-    cin>>en;
-
-    for(int i=0;en[i];i++){
-        cout<<(char)en[i];
-    }
-    cout<<endl;
-
-    char wc1[]="한글화 작업";
-    cout<<wc1<<endl;
-
-    int i=0;
-    while(wc1[i]){
-        cout<<(char)wc1[i];
-        ++i;
-    }
-    cout<<endl;
-
-    const char *wc2="한글화 작업";
-    cout<<wc2<<", 문자 크기: "<<strlen(wc2)<<endl;
+#if defined __linux__ || defined __CYGWN__
+    setlocale(LC_ALL, "ko_KR.utf8");
+    wcout<<L"setlocale(LC_ALL, \"ko_KR.utf8\");"<<endl;
+#elif defined(WIN32)
+    wcout.imbue(std::locale("kor"));
+    wcin.imbue(std::locale)"kor"));
+    cout<<"std::wcout.imbue(std::locale(\"kor\"));"<<endl;
+#else
+    setlocale(LC_ALL, "");
+    wcout<<L"setlocale(LC_ALL,\"Korean\");"<<endl;
+#endif
     
     return 0;
 }
